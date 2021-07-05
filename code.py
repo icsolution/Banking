5c23f35b3ccdb97d519c2b6eb4e0d6db5c0e8d29
Icaro Carvalhais <icarocarvalhais@gmail.com>
2021-07-05 18:57:31 -0400

    Update code.py

--- code.py
+++ code.py
@@ -1,11 +1,20 @@
 import random
+import sqlite3
+
+
+def create_db():
+    conn = sqlite3.connect('card.s3db')
+    cur = conn.cursor()
+    cur.execute('CREATE TABLE IF NOT EXISTS card (id INT, number TEXT, pin TEXT, balance INT DEFAULT 0);')
+    conn.commit()
 
 
 class Bank:
 
     def __init__(self):
-        self.accounts = {}
         self.choice = None
+        self.conn = sqlite3.connect('card.s3db')
+        self.cur = self.conn.cursor()
         self.active()
 
     def active(self):
@@ -22,47 +31,43 @@ class Bank:
                 self.exit()
             self.choice = None
 
-
     def exit(self):
         print('\nBye!')
         quit()
 
     def create_account(self):
-        account_number = str(random.randrange(100000000, 999999999))
-        check_sum = str(random.randrange(0, 9))
-        card_number = '400000' + account_number
-        card_number = self.identifier(card_number)
-        if card_number not in self.accounts:
-            self.accounts[card_number] = dict(pin=str(random.randrange(1000, 9999)), balance=0)
-            print('\nYour account has been created')
-            print(f'Your card number:\n{card_number}')
-            print(f'Your card PIN:\n{self.accounts[card_number]["pin"]}\n')
-            self.active()
-        else:
-            self.active()
+        card_number = self.identifier('400000' + str(random.randrange(100000000, 999999999)))
+        pin= str(random.randrange(1000, 9999))
+        self.update_db(card_number, pin)
+        print('\nYour account has been created')
+        print(f'Your card number:\n{card_number}')
+        print(f'Your card PIN:\n{pin}\n')
+        self.active()
+
+    def update_db(self, card, pin):
+        self.cur.execute(f'INSERT INTO card (number, pin) VALUES ({card}, {pin});')
+        self.conn.commit()
 
     def log_in(self):
-        global current_card
-        current_card = None
-        card_number = input('\nEnter your card number:\n')
+        card = input('\nEnter your card number:\n')
         pin = input('Enter your pin:\n')
-        if card_number in self.accounts and pin == self.accounts[card_number]['pin']:
+        self.cur.execute(f'SELECT * FROM card WHERE number = {card} AND pin = {pin}')
+        inquiry = self.cur.fetchall()
+        if inquiry:
             print('\nYou have successfully logged in!\n')
-            current_card = card_number
-            self.account_status()
+            self.account_status(inquiry)
         else:
             print('\nWrong card number or PIN!\n')
             self.active()
 
-    def account_status(self):
-        global current_card
+    def account_status(self, inquiry):
         print('1. Balance')
         print('2. Log out')
         print('0. Exit')
         choice = input()
         if choice == '1':
-            print(f"\nBalance: {self.accounts[current_card]['balance']}\n")
-            self.account_status()
+            print(f"\nBalance: {inquiry[0][-1]}\n")
+            self.account_status(inquiry)
         elif choice == '2':
             print('\nYou have successfully logged out!\n')
             self.active()
@@ -89,4 +94,6 @@ class Bank:
 
         return number + sum_check
 
+
+create_db()
 b = Bank()
